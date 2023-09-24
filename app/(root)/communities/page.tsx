8996e1694b1596d8390d5b6135718b339d336b19
Yash <yashchandra985@gmail.com>
2023-09-24 22:25:35 +0530

    new commit

--- app/(root)/communities/page.tsx
+++ app/(root)/communities/page.tsx
@@ -1,8 +1,6 @@
 import { currentUser } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
-// import Searchbar from "@/components/shared/Searchbar";
-// import Pagination from "@/components/shared/Pagination";
 import CommunityCard from "@/components/cards/CommunityCard";
 
 import { fetchUser } from "@/lib/actions/user.actions";
